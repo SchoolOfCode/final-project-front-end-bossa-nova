bcf5c28c6ab371dc774e758ee4c73e91a6e2b691
benmhcode <85193753+benmhcode@users.noreply.github.com>
2022-02-18 09:46:34 +0000

    added a basic login page, and added links to the nav bar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,35 @@
 import React from "react";
 import styles from "./Header.module.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+
   return (
     <nav className={styles.Header}>
-      <div>logo home button</div>
-      <button>Log Out</button>
+      <div
+        type="button"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        logo home button
+      </div>
+      <button
+        type="button"
+        onClick={() => {
+          navigate("/login");
+        }}
+      >
+        Log Out
+      </button>
     </nav>
   );
 }
