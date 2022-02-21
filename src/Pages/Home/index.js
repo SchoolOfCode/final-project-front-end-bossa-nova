f413ccefa5a5c91ddfae100e7cd87cf745d1fbae
Chirag Mehta <chiragmehta.uk@gmail.com>
2022-02-21 14:45:50 +0000

    imported table from mui and set table headers

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,6 +1,12 @@
-import React, { useState } from "react";
-import data from "./../data";
-
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 import {
   BrowserRouter as Router,
   Routes,
@@ -10,57 +16,61 @@ import {
   useNavigate,
 } from "react-router-dom";
 
-import styles from "./Home.module.css";
+// import { useState } from "react";
+// import data from "./../data";
 
-function PreviewTable() {
-  const navigate = useNavigate();
 
-  const [jobDisplay, setJobDisplay] = useState(data);
+// import styles from "./Home.module.css";
 
-  return (
-    <div>
-      <button
-        onClick={() => {
-          navigate("/add-new");
-        }}
-      >
-        Add new job
-      </button>
-      <div className={styles.PreviewTable}>
-        <table>
-          <thead>
-            <tr>
-              <th>Job Title</th>
-              <th>Company</th>
-              <th>Salary</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {jobDisplay.map((jobDisplay) => (
-              <tr
-                key={jobDisplay._id}
-                item={jobDisplay}
-                onClick={() => {
-                  navigate("/update" + "/" + jobDisplay._id);
-                }}
-              >
-                <td>{jobDisplay.jobTitle}</td>
-                <td>{jobDisplay.company}</td>
-                <td>
-                  {jobDisplay.minSalary} - {jobDisplay.maxSalary}
-                </td>
-                <td>{jobDisplay.jobStatus}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
-}
+// function PreviewTable() {
 
-export default PreviewTable;
+
+//   const [jobDisplay, setJobDisplay] = useState(data);
+
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           navigate("/add-new");
+//         }}
+//       >
+//         Add new job
+//       </button>
+//       <div className={styles.PreviewTable}>
+//         <table>
+//           <thead>
+//             <tr>
+//               <th>Job Title</th>
+//               <th>Company</th>
+//               <th>Salary</th>
+//               <th>Status</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {jobDisplay.map((jobDisplay) => (
+//               <tr
+//                 key={jobDisplay._id}
+//                 item={jobDisplay}
+//                 onClick={() => {
+//                   navigate("/update" + "/" + jobDisplay._id);
+//                 }}
+//               >
+//                 <td>{jobDisplay.jobTitle}</td>
+//                 <td>{jobDisplay.company}</td>
+//                 <td>
+//                   {jobDisplay.minSalary} - {jobDisplay.maxSalary}
+//                 </td>
+//                 <td>{jobDisplay.jobStatus}</td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default PreviewTable;
 
 /* <main className={styles.Hero}>
       
@@ -68,3 +78,122 @@ export default PreviewTable;
       </CreateForm>
       <PreviewTable />
     </main> */
+
+
+<th>Company</th>
+//               <th>Salary</th>
+//               <th>Status</th>
+    
+    const columns = [
+      { id: 'jobTitle', label: 'Job Title', minWidth: 100 },
+      { id: 'company', label: 'Company', minWidth: 100 },
+      {
+        id: 'salary',
+        label: 'Salary',
+        minWidth: 100,
+        format: (value) => value.toLocaleString('en-UK'),
+      },
+      {
+        id: 'status',
+        label: 'Status',
+        minWidth: 100,
+        format: (value) => value.toLocaleString('en-US'),
+      },
+      {
+        id: 'buttons',
+        label: '',
+        minWidth: 60,
+        align: 'right',
+      },
+    ];
+    
+    function createData(name, code, population, size) {
+      const density = population / size;
+      return { name, code, population, size, density };
+    }
+    
+    const rows = [
+      createData('India', 'IN', 1324171354, 3287263),
+      createData('China', 'CN', 1403500365, 9596961),
+      createData('Italy', 'IT', 60483973, 301340),
+      createData('United States', 'US', 327167434, 9833520),
+      createData('Canada', 'CA', 37602103, 9984670),
+      createData('Australia', 'AU', 25475400, 7692024),
+      createData('Germany', 'DE', 83019200, 357578),
+      createData('Ireland', 'IE', 4857000, 70273),
+      createData('Mexico', 'MX', 126577691, 1972550),
+      createData('Japan', 'JP', 126317000, 377973),
+      createData('France', 'FR', 67022000, 640679),
+      createData('United Kingdom', 'GB', 67545757, 242495),
+      createData('Russia', 'RU', 146793744, 17098246),
+      createData('Nigeria', 'NG', 200962417, 923768),
+      createData('Brazil', 'BR', 210147125, 8515767),
+    ];
+    
+    export default function StickyHeadTable() {
+      const navigate = useNavigate();
+
+      const [page, setPage] = React.useState(0);
+      const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    
+      const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+      };
+    
+      const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+      };
+    
+      return (
+        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+          <TableContainer sx={{ maxHeight: 440 }}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => {
+                    return (
+                      <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                        {columns.map((column) => {
+                          const value = row[column.id];
+                          return (
+                            <TableCell key={column.id} align={column.align}>
+                              {column.format && typeof value === 'number'
+                                ? column.format(value)
+                                : value}
+                            </TableCell>
+                          );
+                        })}
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      );
+    }
+    
